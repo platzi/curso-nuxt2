de68471833b9ccbcc3f4f5d1a0029b5684b817b2
Diana Martinez <diananerdoficial@gmail.com>
2022-07-18 17:57:06 -0500

    Install sass and tailwind styles

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,15 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss">
+.container {
+  @apply m-auto;
+}
+.container .content {
+  @apply flex flex-col justify-center items-center sm:p-2 md:p-4 lg:p-8 xl:p-16;
+  main {
+    @apply max-w-5xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6;
+  }
+}
+</style>
