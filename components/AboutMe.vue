298ae24ac62f7c0cf918b900d4c6acd0c6d745af
Diana Martinez <diananerdoficial@gmail.com>
2022-07-18 18:20:14 -0500

    Add styles to indec components

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,3 +1,15 @@
 <template>
   <div class="about-me">About Me</div>
 </template>
+
+<style lang="scss" scoped>
+.about-me {
+  @apply w-full flex flex-col justify-center items-center px-4 py-16 bg-gray-200;
+  h1 {
+    @apply font-black text-4xl text-gray-900 pb-2;
+  }
+  p {
+    @apply text-lg py-2;
+  }
+}
+</style>
--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -4,8 +4,34 @@
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .article-card {
-  @apply border border-red-700;
+  @apply block bg-white sm:border sm:rounded-lg p-4;
+  h3 {
+    @apply font-bold text-xl mb-2 mt-0;
+  }
+  div.author {
+    @apply my-2;
+    p {
+      @apply m-0;
+    }
+    small {
+      @apply italic;
+    }
+  }
+  figure {
+    img {
+      @apply w-full;
+    }
+  }
+  p {
+    @apply my-4 font-serif;
+  }
+  div.actions {
+    @apply flex gap-2;
+    .btn {
+      @apply w-full text-center border rounded-full px-8 py-2 bg-gray-200 hover:bg-gray-300;
+    }
+  }
 }
 </style>
