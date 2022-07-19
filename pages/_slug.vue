a8944e1d4cdd0ae2d927005ed84c03666d78244b
Diana Martinez <diananerdoficial@gmail.com>
2022-07-19 15:47:28 -0500

    Add post view

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -4,29 +4,26 @@
       <h1>{{ post.title }}</h1>
       <div class="author">
         <p>Por {{ post.author }}</p>
-        <small
-          >Fecha de publicación:
-          {{ post.updated }}</small
-        >
+        <small>Fecha de publicación: {{ post.updated }}</small>
       </div>
       <p>{{ post.description }}</p>
       <figure>
-        <img
-          :src="post.cover"
-          :alt="post.cover"
-        />
+        <img :src="post.cover" :alt="post.cover" />
         <figcaption>Portada - {{ post.title }}</figcaption>
       </figure>
-      <div class="markdown">
-        {{ post.content }}
-      </div>
+      <VueMarkdown class="markdown">{{ post.content }}</VueMarkdown>
     </article>
   </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+
 export default {
   name: 'ArticlePage',
+  components: {
+    VueMarkdown,
+  },
   data() {
     return {
       post: {
@@ -36,14 +33,15 @@ export default {
         updated: '8/06/2022',
         description: 'Lorem ispum dolor sit amet',
         cover: 'https://via.placeholder.com/1024x420',
-        content: `
-          # Title
-          ## Second title
-
-          Lorem ipsum dolor sit amet
-        `,
-      }
-    };
+        content: '# Title\n\n## Second title\n\nLorem ipsum dolor sit amet',
+      },
+    }
+  },
+  head() {
+    return {
+      title: this.post?.title,
+      meta: [{ name: 'description', content: this.post?.description || '' }],
+    }
   },
 }
 </script>
