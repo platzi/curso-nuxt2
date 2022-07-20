f603d3ed5785e589b731339295cf2ea3b4470f36
Diana Martinez <diananerdoficial@gmail.com>
2022-07-20 15:58:09 -0500

    Add comment system components

---
+++ components/CommentItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="comment">
+    <p class="meta">
+      <span>{{ author }} - {{ authorEmail }}</span>
+      <span>{{ new Date(created).toLocaleString() }}</span>
+    </p>
+    <p class="content">{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentItem',
+  props: {
+    author: {
+      type: String,
+      default: 'Anonimo',
+    },
+    authorEmail: {
+      type: String,
+      default: 'unknown email',
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+    created: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.comment {
+  @apply bg-white px-4 py-4;
+  .meta {
+    @apply flex flex-col md:flex-row justify-between items-start md:items-center text-gray-400;
+  }
+  .content {
+    @apply mt-2;
+  }
+}
+</style>
